4d11a94013c88378236337459ad84e896a307d0d
Nishant Patil <142099904+Nishant0121@users.noreply.github.com>
2024-09-15 10:08:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173", // Replace with your frontend's actual origin
+    origin: "https://fin-wise-bice.vercel.app", // Replace with your frontend's actual origin
   })
 );
 
